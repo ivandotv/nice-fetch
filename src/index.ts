d0a5d58230075ad1edd294204537d3d84a08cdca
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-02-13 21:55:01 +0100

    feat: initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
-export function demo(): void {
-  if (__DEV__) {
-    console.log('this should show only in dev')
-  }
+type KeyToBodyResponse = {
+  blob: Blob
+  formData: FormData
+  arrayBuffer: ArrayBuffer
+  text: string
+  json: any
 }
-demo()
+
+export default function ffetch<
+  TExpectedData extends keyof KeyToBodyResponse = keyof KeyToBodyResponse
+>(
+  reqInfo: RequestInfo,
+  reqInit?: RequestInit,
+  // @ts-expect-error - suppress "different subtype constraint" error  because we can't have default value for a generic type
+  content: TExpectedData = 'json'
+): Promise<[KeyToBodyResponse[typeof content], Response]> {
+  return new Promise((resolve, reject) => {
+    return fetch(reqInfo, reqInit)
+      .then((res) => {
+        if (res.ok) {
+          res[content]().then(resolve).catch(reject)
+        } else {
+          reject(res)
+        }
+      })
+      .catch(reject)
+  })
+}
