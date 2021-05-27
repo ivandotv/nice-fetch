99d005d987f7c9e721deca08a5298e0361856716
ivandotv <390700+ivandotv@users.noreply.github.com>
2021-05-27 22:10:58 +0200

    eslint runs prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/@typescript-eslint'
+    'prettier'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
